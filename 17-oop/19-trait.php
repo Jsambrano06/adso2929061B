d8fef31ea257f0db452ca6a7a5c838a765ef1666
Jsambrano06 <sambranojacobo@gmail.com>
2025-11-14 21:00:28 -0500

    folder oop

---
+++ 17-oop/19-trait.php
@@ -0,0 +1,14 @@
+<?php
+
+    $tittle = "09 - Oper Logic";
+    $descripcion = "Perform logic operations on variables";
+
+include 'template/header.php';
+    echo '<section>';
+    
+?>
+
+
+    <?php
+include 'template/footer.php';
+?>
