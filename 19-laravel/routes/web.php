a8ab96853a0ca6117149a570c93007b56ebf57bd
Jsambrano06 <sambranojacobo@gmail.com>
2026-02-27 21:03:11 -0500

    modify

--- 19-laravel/routes/web.php
+++ 19-laravel/routes/web.php
@@ -1,15 +1,23 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Models\User;
-use App\Models\Pet;
-use Carbon\Carbon;
-use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
 Route::get('hello', function () {
     return "<h1>Hello Laravel 🚀</h1>";
 });
